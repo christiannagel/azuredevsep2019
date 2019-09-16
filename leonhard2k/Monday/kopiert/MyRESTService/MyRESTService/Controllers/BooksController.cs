92233a079ce6aacb04eea7fc5d454b64e7c324ef
leonhard Pfeiffer-Vogl <leonhard.pfeiffer-vogl@post.at>
2019-09-16 16:40:15 +0200

    sd

---
+++ leonhard2k/Monday/kopiert/MyRESTService/MyRESTService/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRESTService.Models;
+using MyRESTService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRESTService.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService ?? throw new ArgumentNullException(nameof(booksService));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_booksService.GetBooks());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(Book), 200)]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> PostBook([FromBody] Book book)
+        {
+            book = _booksService.AddBook(book);
+            return Ok(book);
+        }
+    }
+}
