b92fd42170e59cdc60840f1ab02105e154bd3f57
Christian Nagel <christian@christiannagel.com>
2019-09-17 13:00:32 +0200

    web app

---
+++ Tuesday/MyWebApp/MyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+        public HomeController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("MyHomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogTrace("Index called");
+
+            _logger.LogError("Some error occurred");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
