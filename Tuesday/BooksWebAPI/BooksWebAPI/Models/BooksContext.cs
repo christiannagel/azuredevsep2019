afd0ec8aaf2de2b7d22b4e0f6c6f7c2462140bf2
Christian Nagel <christian@christiannagel.com>
2019-09-17 13:32:34 +0200

    web api with ef core

---
+++ Tuesday/BooksWebAPI/BooksWebAPI/Models/BooksContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BooksWebAPI
+{
+    public partial class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.BookId);
+
+                entity.Property(e => e.Publisher).HasMaxLength(25);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+        }
+    }
+}
